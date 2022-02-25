c0ba51878fb5a3fc299bd92352ee2e6fea1a7e50
Ольга Воронюк <helga-61rus@yandex.ru>
2022-02-25 15:48:28 +0300

    добавлен метакласс для модели подписок

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models import UniqueConstraint
 
 User = get_user_model()
 
@@ -83,10 +82,14 @@ class Follow(models.Model):
         on_delete=models.CASCADE,
         related_name='following'
     )
-    UniqueConstraint(
-        fields=['user', 'author'],
-        name='subscription_unique'
-    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author'],
+                name='subscription_unique'
+            ),
+        ]
 
     def __str__(self):
         return f'Подписчик: {self.user}, автор: {self.author}'
